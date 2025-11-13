5166a5df6ba17ce95f2902928de3e350e660720e
Chayma Romdhane <chayma.romdhane@supcom.tn>
2025-11-13 21:15:03 +0100

    Push SupFlow project to test branch

--- src/app/app.spec.ts
+++ src/app/app.spec.ts
@@ -1,23 +1,24 @@
 import { provideZonelessChangeDetection } from '@angular/core';
 import { TestBed } from '@angular/core/testing';
-import { App } from './app';
+import { app } from './app';
+import { Login } from './pages/login/login';
 
-describe('App', () => {
+describe('app', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [App],
+      imports: [app],
       providers: [provideZonelessChangeDetection()]
     }).compileComponents();
   });
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(App);
+    const fixture = TestBed.createComponent(Login);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
 
   it('should render title', () => {
-    const fixture = TestBed.createComponent(App);
+    const fixture = TestBed.createComponent(Login);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
     expect(compiled.querySelector('h1')?.textContent).toContain('Hello, SupFlow');
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,29 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { RouterLink } from '@angular/router';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterLink, FormsModule],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrls: ['./app.css'],
 })
-export class App {
-  protected readonly title = signal('SupFlow');
+export class app {
+  showPwd = false;
+  form = {
+    email: '',
+    password: '',
+    remember: false,
+  };
+
+  togglePwd(): void {
+    this.showPwd = !this.showPwd;
+  }
+
+  onSubmit(): void {
+    // TODO: brancher AuthService
+    console.log('Form submitted', this.form);
+  }
 }
